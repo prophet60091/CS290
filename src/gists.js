fa4981122edc7a85718da17a4ba3262c2aff3b83
Robert Jackson <jackrobe@onid.oregonestate.edu>
2015-04-24 21:14:26 -0600

    working!
    The filter function is still not working. Going to brach and read.

--- src/gists.js
+++ src/gists.js
@@ -1,23 +1,24 @@
-
 //some date stuff for git requests
 var dateOff = (  60 * 1000); // minute
 var d = new Date();
 d.setTime(d.getTime() - dateOff);
 gitDate = d.toISOString();
-var per_page = 30;
-var gitURL = 'https://api.github.com/gists/public';
 
-function connect(url, queryType) {
+var gitURL = 'https://api.github.com/gists/public';
+var respArray = [];
+
+
+function connect(url) {
 
     //did we get anything
-    if(!url){
-        url = 'https://api.github.com/gists/public?per_page=' + per_page ;
+    if (!url) {
+        url = 'https://api.github.com/gists/public?per_page=' + localStorage.getItem('per_page');
     }
 
     var req = new XMLHttpRequest();
 
     // make sure it got somewhere
-    if (!req)console.log( 'Something broke with the request to ' +  url);
+    if (!req)console.log('Something broke with the request to ' + url);
 
     req.onreadystatechange = state;
     req.open('GET', url);
@@ -30,17 +31,18 @@ function connect(url, queryType) {
             //check the response
             if (req.status === 200) {
                 // everything is good, the response is received
-                console.log("it works");
-                response = JSON.parse(req.responseText);
 
-                if(response !== '') {
+                response = JSON.parse(req.responseText);
+                localStorage.setItem('firstrep', response);
+                if (response !== '') {
                     console.log('I have a response');
                     //depending on how we set the url, we may need to change the object
 
-                    displayGists(response, 'gists');
+                    dispArray(arrayG(response), 'arrayVersion');
+                    dispArray(JSON.parse(localStorage.getItem('favs')), 'favs');
                     return response;
                 }
-            }else{
+            } else {
 
                 console.log('Received a response other than 200');
                 console.log(req.statusText);
@@ -49,110 +51,133 @@ function connect(url, queryType) {
     }
 }
 
-function addFav() {
+function addFav(sender) {
+    var fav = [];
+    var g = [];
 
+    if (!localStorage.getItem('favs') || localStorage.getItem('favs') == '') {
+
+        fav.push(g[sender.target.id] = new Gist(respArray[sender.target.id].url, respArray[sender.target.id].desc, respArray[sender.target.id].lang, sender.target.id));
+        localStorage.setItem('favs', JSON.stringify(fav));
+        document.getElementById(sender.target.id).parentElement.style.display = "none";
+
+        //remove the pointer from our array
+        delete respArray[sender.target.id];
+        dispArray(JSON.parse(localStorage.getItem('favs')), 'favs');
+
+    } else {
+        fav = JSON.parse(localStorage.getItem('favs'));
+
+        //add it to the favorites if it's not already there
+        if (fav.indexOf(sender.target.id) === -1) {
+
+            //push it and store it locally
+            fav.push(g[sender.target.id] = new Gist(respArray[sender.target.id].url, respArray[sender.target.id].desc, respArray[sender.target.id].lang, sender.target.id));
+            localStorage.setItem('favs', JSON.stringify(fav)); //add locally
+
+            //remove the pointer from our array
+            delete respArray[sender.target.id];
+
+            //hide it from the display
+            document.getElementById(sender.target.id).parentElement.style.display = "none";
+
+            //update the display
+            dispArray(JSON.parse(localStorage.getItem('favs')), 'favs');
+
+        } else {
+            alert('That is already in your favorites')
+        }
+    }
+}
+
+//Constructor funciton to make a list ob objects
+function Gist(url, desc, lang, id) {
+    this.url = url;
+    this.desc = desc;
+    this.lang = lang;
+    this.id = id;
+}
+
+
+// Makes an Array out of the textresponse.
+//@param a JSON.parse of the responseText
+function arrayG(rsp) {
+
+    var fileLang;
+
+    rsp.forEach(function (rowData) {
+
+        for (file in rowData.files) {
+            var languages = [];
+
+            if(rowData.files.hasOwnProperty(file))
+                languages.push(rowData.files[file].language);
+        }
+
+        if (languages.length >= 1) {
+            fileLang = languages.join(', ')
+        } else {
+            fileLang = 'n/a'
+        }
+        respArray[rowData.id] = new Gist(rowData.html_url, rowData.description, languages, rowData.id);
+
+    });
+
+    return respArray;
+}
+//Displays the Array from ArrayG
+//@param array = A Json.parsed  array;
+//@param where  = what parent element in the DOM
+function dispArray(array, where) {
+
+    document.getElementById(where).innerHTML = '';
+
+    //start the list
+    var display = document.getElementById(where);
+    var list = document.createElement("ul");
+
+    for (key in array) {
+
+        var row = document.createElement("li");
+
+        //The link to the actual GIST
+        var rowLink = document.createElement("a");
+        rowLink.href = array[key].url;
+        rowLink.className = 'gitLink';
+        rowLink.target = "_blank";
+
+        // Description changed if it's empty.
+        var fileDesc = document.createTextNode(array[key].desc);
+        if (array[key].desc === '' || array[key].desc === null) {
+            fileDesc = document.createTextNode('No Description Entered')
+        }
+        /// A link to add stuff to the Fav list
+        var addtoFav = document.createElement("a");
+        addtoFav.onclick = addFav;
+        addtoFav.href = "javascript:void(0)";
+        addtoFav.className = "addLink";
+        addtoFav.target = "_blank";
+        addtoFav.id = array[key].id;
+        var addtoFavLink = document.createTextNode(' + ');
+
+        //Appendages
+        addtoFav.appendChild(addtoFavLink);
+        row.appendChild(rowLink);
+        rowLink.appendChild(fileDesc);
+        row.appendChild(addtoFav);
+        list.appendChild(row);
+    }
+    //finally add all that to the top element
+    display.appendChild(list);
 
 }
 
 
-
-
-//grabs relevant data in the response
-//@param rsp, takes a XMLHTTPResponse object
-//@param where, where to display the list i.e favorites or
-function displayGists(rsp, where) {
-
-        if(!rsp){ rsp = connect().response}
-        if(!where){where = 'gists'}
-        //clear out the space
-        //allowing successive calls to replace existing data
-        document.getElementById(where).innerHTML = '';
-
-        //start the list
-        var display = document.getElementById("gists");
-        var list = document.createElement("ul");
-
-            console.log(rsp);
-            rsp.forEach(function (rowData) {
-
-                var row = document.createElement("li");
-
-                var rowLink = document.createElement("a");
-                    rowLink.href = rowData.html_url;
-                    rowLink.className = 'gitLink';
-                    rowLink.target = "_blank";
-
-                var fileDesc = document.createTextNode(rowData.description);
-                if(rowData.description === '' || rowData.description === null){
-                    fileDesc = document.createTextNode('No Decription Entered')
-                }
-                var addtoFav = document.createElement("a");
-                    addtoFav.onclick = addFav;
-                    addtoFav.className = "addLink";
-                    addtoFav.target = "_blank";
-                    addtoFav.id = rowData.id;
-                    var addtoFavLink = document.createTextNode(' + ');
-                    addtoFav.appendChild(addtoFavLink);
-
-                    //addtoFav.appendChild(document.createTextNode('Add'));
-
-                     //fileDesc.appendChild(addtoFav);
-                //var cell_date = document.createTextNode( rowData.updated_at );
-                /*var listFiles = document.createElement("ul");
-
-                //Grab the file Names and Info
-                for (file in rowData.files) {
-
-                    var rowFiles = document.createElement("li");
-                    var fileName = document.createElement("div");
-                        fileName.className = "fileName";
-                        fileName.innerHTML = rowData.files[file].filename;
-
-                    var fileLink = document.createElement("a");
-                        fileLink.href = rowData.files[file].raw_url;
-                        fileLink.class = "fileLink";
-
-                    var fileLang = document.createTextNode(rowData.files[file].language);
-                    rowFiles.appendChild(fileName);
-                    //rowFiles.appendChild(fileLink);
-                    rowFiles.appendChild(fileLang);
-                    listFiles.appendChild(rowFiles);
-                }
-                */
-
-                row.appendChild(rowLink);
-
-                rowLink.appendChild(fileDesc);
-                //rowLink.insertBefore(addtoFav, fileDesc);
-                row.appendChild(addtoFav);
-                //row.appendChild(listFiles);
-                list.appendChild(row);
-            });
-            //put in the retunred information
-            display.appendChild(list);
-
-}
-
-//function addLang(){
-//
-//
-//    rsp.forEach(function (rowData) {
-//
-//        //grab the file Names and Info
-//        for (file in rowData.files) {
-//
-//            var fileLang = document.createTextNode(rowData.files[file].language);
-//
-//    });
-//
-//
-//}
-
-function filter(clear){
+function filter(clear) {
     var newurl = '';
 
 
-    if(!clear) {
+    if (!clear) {
         var filters = [];
         var pages;
 
@@ -164,13 +189,13 @@ function filter(clear){
 
         }
         //if there are filters add them to the request
-        if (filters.length >= 1 ) {
+        if (filters.length >= 1) {
 
             newurl = 'https://api.github.com/gists/public?q=';
             newurl += 'language=';
-            var languages  = filters.join(' OR ');
+            var languages = filters.join(' OR ');
             newurl += encodeURIComponent(languages);
-            newurl += '&per_page=' + per_page;
+            newurl += '&per_page=' + localStorage.getItem('per_page');
             console.log(newurl);
         }
 
@@ -179,27 +204,25 @@ function filter(clear){
         //newurl += 'per_page=' + per_page;
         //if the page changed add that too api.github.com/search/repositories?q=
 
-    }else{
+    } else {
         connect();
 
     }
 
 }
 
-function setPPage(){
+function setPPage() {
 
-    per_page = document.getElementById('pages').value;
+    localStorage.setItem('per_page', document.getElementById('pages').value);
     connect();
 }
 
-window.onload = function(){
-   console.log('Connecting...');
+window.onload = function () {
 
+    console.log('Connecting...');
+    connect('https://api.github.com/gists/public?per_page=' + localStorage.getItem('per_page'));
+    document.getElementById('pages').value = localStorage.getItem('per_page');
 
-   connect('https://api.github.com/gists/public?per_page='+ per_page);
-
-    document.getElementById('pages').value = per_page ;
-    //displayGists(response, 'gists');
 
 };
 
